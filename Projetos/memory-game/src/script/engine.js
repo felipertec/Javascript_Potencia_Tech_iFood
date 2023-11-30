d19a5377b531cded3d60062d9606546f21cbcdeb
felipertec <felipe.rtec@gmail.com>
2023-11-30 08:03:26 -0300

    Criado cards

---
+++ Projetos/memory-game/src/script/engine.js
@@ -0,0 +1,27 @@
+const emojis = [
+    "😺",
+    "😺",
+    "🦝",
+    "🦝",
+    "🦊",
+    "🦊",
+    "🐶",
+    "🐶",
+    "🐵",
+    "🐵",
+    "🐮",
+    "🐮"
+];
+
+
+let openCard = [];
+
+
+let shuffleEmojis = emojis.sort(()=>(Math.random() > 0.5) ? 2 : -1);
+
+for(let i = 0; i < emojis.length; i++){
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    document.querySelector(".game").appendChild(box)
+}
